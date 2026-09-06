026407bdf761f823b7f24de027d96cbb2db28bde
noah <harrington.noah@tutamail.com>
2026-09-06 01:51:58 -0600

    Added two new sounds for clicking the start and quit button respectively.

--- main_menu.gd
+++ main_menu.gd
@@ -2,6 +2,8 @@ extends Control
 
 var menu_music = preload("res://music/Main Menu - Dark.wav")
 var button_sound = preload("res://sounds/menu_7.wav")
+var quit_sound = preload("res://sounds/menu_21.wav")
+var play_sound = preload("res://sounds/menu_14.wav")
 
 func _process(delta: float) -> void:
 	if !$menu_music.is_playing():
@@ -25,3 +27,14 @@ func _on_quit_mouse_entered() -> void:
 	if !$button_sound.is_playing():
 		$button_sound.stream = button_sound
 		$button_sound.play()
+
+
+func _on_start_button_focus_entered() -> void:
+	if !$play_sound.is_playing():
+		$play_sound.stream = play_sound
+		$play_sound.play()
+
+func _on_quit_focus_entered() -> void:
+	if !$quit_sound.is_playing():
+		$quit_sound.stream = quit_sound
+		$quit_sound.play()
